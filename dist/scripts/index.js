9ee421b6a7b0ea47bbf8403f4f1cb4ec08ea99a0
Asadex <19artur98@gmail.com>
2021-02-15 17:30:50 +0100

    solved scroll bug in mobiles

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,18 +1,21 @@
 const HAMBURGER = document.getElementById('hamburger-menu');
 const MENU = document.getElementById('menu');
 const URBAN = document.getElementById('urban');
+const NATURE = document.getElementById('nature');
 const CLOSE = document.getElementById('close');
 let menuStatus = false;
 
 HAMBURGER.addEventListener('click',function(){
     URBAN.style.left = '55vw';
     document.getElementById('body').style.overflow = 'hidden';
+    document.getElementById('body').style.position = 'fixed';
     menuStatus = true;
 })
 
 CLOSE.addEventListener('click',function(){
     URBAN.style.left = '0';
     document.getElementById('body').style.overflowY = 'scroll';
+    document.getElementById('body').style.position = 'initial';
     menuStatus = false;
 })
 
@@ -20,6 +23,7 @@ window.addEventListener('click', function(e){
     if(e.target === URBAN){
         URBAN.style.left = '0';
         document.getElementById('body').style.overflowY = 'scroll';
+        document.getElementById('body').style.position = 'initial';
         menuStatus = false;
     }
 })
