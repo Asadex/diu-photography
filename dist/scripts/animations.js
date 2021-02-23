f32957281fc90fda849e734ac5b29cf6b2674a5d
Asadex <19artur98@gmail.com>
2021-02-23 12:42:34 +0100

    Bug animations...

--- dist/scripts/animations.js
+++ dist/scripts/animations.js
@@ -5,7 +5,6 @@ sr.reveal('.section-title',{
     duration: 1000,
     origin: 'bottom',
     distance: '50px',
-    cleanup: true,
     reset: true,
     viewFactor: 0.5,
 })
@@ -15,7 +14,6 @@ sr.reveal('.cool-description',{
     origin: 'bottom',
     duration: 1000,
     distance: '50px',
-    cleanup: true,
     viewFactor: 0.8,
-    delay: 300,
+    delay: 400,
 })
